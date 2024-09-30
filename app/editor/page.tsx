a8360f24a1ae8a60a06d76fee75e10f7d12ee6a2
kvtemadden <74623315+kvtemadden@users.noreply.github.com>
2024-09-30 17:18:46 +0100

    add tabs for video & screenshot

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,7 +1,35 @@
-import EditorNotAvailable from "@/components/shared/NotAvailable"
-import MockupEditor from "@/components/shared/Editor"
-import { siteConfig } from "@/config/config"
+"use client";
+
+import EditorNotAvailable from "@/components/shared/NotAvailable";
+import MockupEditor from "@/components/shared/Editor";
+import { siteConfig } from "@/config/config";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Header from "@/components/layout/Header";
 
 export default function EditorPage() {
-  return siteConfig.isEditorActive ? <MockupEditor /> : <EditorNotAvailable />
+  // return siteConfig.isEditorActive ? <MockupEditor /> : <EditorNotAvailable />;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+
+      <Tabs defaultValue="screenshot" className="w-full max-w-screen-lg">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="screenshot">Screenshot</TabsTrigger>
+          <TabsTrigger value="video">Video</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="screenshot" className="space-y-4 pt-3">
+          {siteConfig.isEditorActive ? (
+            <MockupEditor />
+          ) : (
+            <EditorNotAvailable />
+          )}
+        </TabsContent>
+
+        <TabsContent value="video" className="space-y-4">
+          <EditorNotAvailable />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
