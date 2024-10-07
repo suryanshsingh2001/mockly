296bff9c27484edf2af694cb408136ab18d132c8
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2024-10-07 09:33:13 +0530

    (Add) FAQ Section

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,74 @@
 export const FontFamilyNames: string[] = [
-  'Arial',
-  'Impact',
-  'Georgia',
-  'Trebuchet MS',
-  'Courier New',
-  'Comic Sans MS',
+  "Arial",
+  "Impact",
+  "Georgia",
+  "Trebuchet MS",
+  "Courier New",
+  "Comic Sans MS",
 ];
 
-export type FontFamilyName = typeof FontFamilyNames[number];
+export type FontFamilyName = (typeof FontFamilyNames)[number];
+
+export const faqs = [
+  {
+    question: "What is Mockly?",
+    answer:
+      "Mockly is an open-source tool designed to help developers create professional-looking screenshots and mockups quickly and easily. It's perfect for developers who want to produce high-quality visuals without needing to dive into complex design tools.",
+  },
+  {
+    question: "Do I need to sign up to use Mockly?",
+    answer:
+      "No, Mockly does not require any login or account creation. You can start using the tool right away without the need for any sign-up process.",
+  },
+  {
+    question: "Is Mockly free to use?",
+    answer:
+      "Yes, Mockly is completely free to use, and there are no hidden fees or watermarks. You get access to all its powerful features without any cost.",
+  },
+  {
+    question: "Is my data secure when using Mockly?",
+    answer:
+      "Yes, it is. Mockly does not store any of your images, text, or designs. All processing is done in real-time, and nothing is saved on our servers.",
+  },
+  {
+    question: "What formats does Mockly support for screenshots?",
+    answer:
+      "You can upload images in common formats like PNG, JPEG, and SVG. These formats are widely supported and make it easy to incorporate your screenshots into the tool.",
+  },
+  {
+    question: "How can I customize background image?",
+    answer:
+      "Custom backgrounds are currently not available, but are planned for future updates. Stay tuned for updates, or if you'd like, feel free to contribute to the implementation!",
+  },
+  {
+    question: "Can I preview my designs on multiple devices?",
+    answer:
+      "Yes! Mockly allows you to preview your designs on various screen sizes and devices. This feature ensures that your mockups will look great on any platform.",
+  },
+
+  {
+    question: "Can I collaborate with others on Mockly?",
+    answer:
+      "As of now, Mockly doesn't have real-time collaboration features, but they are part of the future roadmap! Stay tuned for updates.",
+  },
+  // {
+  //   question: "Does Mockly have a plugin system?",
+  //   answer: "Not yet, but we plan to introduce a plugin system in the future that will allow developers to extend the tool's functionality by creating and integrating custom plugins."
+  // },
+  {
+    question: "How do I contribute to the project?",
+    answer:
+      "Mockly is open-source and welcomes contributions! You can check out our contribution guidelines and participate in Hacktoberfest or submit features, bug fixes, and more throughout the year.",
+  },
+
+  {
+    question: "Will Mockly support video editing?",
+    answer:
+      "Yes, we are planning to add video editing capabilities in the future, so you can create stunning video content with the same ease as creating screenshots.",
+  },
+  {
+    question: "What if I need help with Mockly?",
+    answer:
+      "For any questions or help, feel free to reach out through our GitHub Discussions or contact us via our project website or GitHub repository.",
+  },
+];
